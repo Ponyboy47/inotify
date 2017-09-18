6a88dc58d4902a215e235d5a43099aae0f2adac9
Jacob Williams <ponyboy47@gmail.com>
2017-09-17 21:01:02 -0600

    Added more initializers for inotify_init1 with flags and made a new Watcher struct for inotify watches (instead of just the tuple I previously used), added a callback function to be used when events get triggered

--- Sources/inotify.swift
+++ Sources/inotify.swift
@@ -7,6 +7,7 @@ public typealias FileDescriptor = Int32
 public typealias WatchDescriptor = Int32
 /// The type used for inotify flags
 public typealias InotifyFlagType = Int32
+/// An enum with all the possible flags for inotify_init1
 public enum InotifyFlag: InotifyFlagType {
     /**
         When the none flag is used, the behavior will be the same as the
@@ -34,8 +35,27 @@ public typealias FileSystemEventType = UInt32
 public struct Inotify {
     /// The file descriptor created by inotify_init()
     private let fileDescriptor: FileDescriptor
-    /// A tuple used to track the paths being watched
-    private var watchingDescriptors: [(WatchDescriptor, FilePath, FileSystemEventType)] = []
+    /// An array of Watcher structs for each path being watched
+    private var watchers: [Watcher] = []
+
+    /// A struct for inotify watched paths
+    public struct Watcher {
+        /// The descriptor used to identify the watcher
+        fileprivate let descriptor: WatchDescriptor
+        /// The file path being watched
+        fileprivate let path: FilePath
+        /// The event mask that inotify is watching for
+        fileprivate let mask: FileSystemEventType
+        /// The callback to use when an event gets triggered
+        fileprivate let callback: (inotify_event) -> ()
+
+        public init(_ descriptor: WatchDescriptor, _ path: FilePath, _ mask: FileSystemEventType, _ callback: @escaping (inotify_event) -> ()) {
+            self.descriptor = descriptor
+            self.path = path
+            self.mask = mask
+            self.callback = callback
+        }
+    }
 
     /**
         Default initializer. Simply calls inotify_init()
@@ -59,6 +79,67 @@ public struct Inotify {
             }
             throw InotifyError.InitError.unknownInitFailure
         }
+    }
+
+    /**
+        Initialize and watch for the specified events on all the paths
+
+        - Parameters:
+            - paths: An array of paths to watch
+            - events: An array of the events for which to monitor on each of the paths
+            - actionOnEvent: The callback to use for when an event is triggered on the paths
+
+        - Throws: If the inotify_init() file descriptor is less than 0
+        - Throws: If the inotify_add_watch(fd, path, mask) returned a file descriptor less than 0 for one of the paths
+    */
+    public init(watching paths: [FilePath], for events: [FileSystemEvent], actionOnEvent callback: @escaping (inotify_event) -> ()) throws {
+        try self.init()
+        try self.watch(paths: paths, for: events, actionOnEvent: callback)
+    }
+
+    /**
+        Initialize and watch for the specified events on all the paths
+
+        - Parameters:
+            - paths: An array of paths to watch
+            - event: A single event for which to monitor on each of the paths
+            - actionOnEvent: The callback to use for when an event is triggered on the paths
+
+        - Throws: If the inotify_init() file descriptor is less than 0
+        - Throws: If the inotify_add_watch(fd, path, mask) returned a file descriptor less than 0 for one of the paths
+    */
+    public init(watching paths: [FilePath], for event: FileSystemEvent, actionOnEvent callback: @escaping (inotify_event) -> ()) throws {
+        try self.init(watching: paths, for: [event], actionOnEvent: callback)
+    }
+
+    /**
+        Initialize and watch for the specified events on all the paths
+
+        - Parameters:
+            - path: The path to watch
+            - events: An array of the events for which to monitor on the path
+            - actionOnEvent: The callback to use for when an event is triggered on the path
+
+        - Throws: If the inotify_init() file descriptor is less than 0
+        - Throws: If the inotify_add_watch(fd, path, mask) returned a file descriptor less than 0 for one of the paths
+    */
+    public init(watching path: FilePath, for events: [FileSystemEvent], actionOnEvent callback: @escaping (inotify_event) -> ()) throws {
+        try self.init(watching: [path], for: events, actionOnEvent: callback)
+    }
+
+    /**
+        Initialize and watch for the specified events on all the paths
+
+        - Parameters:
+            - path: The path to watch
+            - event: A single event for which to monitor on the path
+            - actionOnEvent: The callback to use for when an event is triggered on the path
+
+        - Throws: If the inotify_init() file descriptor is less than 0
+        - Throws: If the inotify_add_watch(fd, path, mask) returned a file descriptor less than 0 for one of the paths
+    */
+    public init(watching path: FilePath, for event: FileSystemEvent, actionOnEvent callback: @escaping (inotify_event) -> ()) throws {
+        try self.init(watching: [path], for: [event], actionOnEvent: callback)
     }
 
     /**
@@ -96,7 +177,7 @@ public struct Inotify {
     /**
         Initializer with an inotify flag. Calls inotify_init1(flags)
 
-        - Parameter flag: An single flag to pass to inotify_init1(flags)
+        - Parameter flag: A single flag to pass to inotify_init1(flags)
 
         - Throws: When the file descriptor returned by inotify_init1() is less than 0
     */
@@ -108,57 +189,129 @@ public struct Inotify {
         Initialize and watch for the specified events on all the paths
 
         - Parameters:
+            - flags: The inotify flags to use in inotify_init1(flags)
             - paths: An array of paths to watch
             - events: An array of the events for which to monitor on each of the paths
+            - actionOnEvent: The callback to use for when an event is triggered on the paths
 
         - Throws: If the inotify_init() file descriptor is less than 0
         - Throws: If the inotify_add_watch(fd, path, mask) returned a file descriptor less than 0 for one of the paths
     */
-    public init(watching paths: [FilePath], for events: [FileSystemEvent]) throws {
-        try self.init()
-        try self.watch(paths: paths, for: events)
+    public init(flags: [InotifyFlag], watching paths: [FilePath], for events: [FileSystemEvent], actionOnEvent callback: @escaping (inotify_event) -> ()) throws {
+        try self.init(flags: flags)
+        try self.watch(paths: paths, for: events, actionOnEvent: callback)
     }
 
     /**
         Initialize and watch for the specified events on all the paths
 
         - Parameters:
+            - flags: The inotify flags to use in inotify_init1(flags)
             - paths: An array of paths to watch
             - event: A single event for which to monitor on each of the paths
+            - actionOnEvent: The callback to use for when an event is triggered on the paths
 
         - Throws: If the inotify_init() file descriptor is less than 0
         - Throws: If the inotify_add_watch(fd, path, mask) returned a file descriptor less than 0 for one of the paths
     */
-    public init(watching paths: [FilePath], for event: FileSystemEvent) throws {
-        try self.init(watching: paths, for: [event])
+    public init(flags: [InotifyFlag], watching paths: [FilePath], for event: FileSystemEvent, actionOnEvent callback: @escaping (inotify_event) -> ()) throws {
+        try self.init(flags: flags, watching: paths, for: [event], actionOnEvent: callback)
     }
 
     /**
         Initialize and watch for the specified events on all the paths
 
         - Parameters:
+            - flags: The inotify flags to use in inotify_init1(flags)
             - path: The path to watch
             - events: An array of the events for which to monitor on the path
+            - actionOnEvent: The callback to use for when an event is triggered on the path
 
         - Throws: If the inotify_init() file descriptor is less than 0
         - Throws: If the inotify_add_watch(fd, path, mask) returned a file descriptor less than 0 for one of the paths
     */
-    public init(watching path: FilePath, for events: [FileSystemEvent]) throws {
-        try self.init(watching: [path], for: events)
+    public init(flags: [InotifyFlag], watching path: FilePath, for events: [FileSystemEvent], actionOnEvent callback: @escaping (inotify_event) -> ()) throws {
+        try self.init(flags: flags, watching: [path], for: events, actionOnEvent: callback)
     }
 
     /**
         Initialize and watch for the specified events on all the paths
 
         - Parameters:
+            - flags: The inotify flags to use in inotify_init1(flags)
             - path: The path to watch
             - event: A single event for which to monitor on the path
+            - actionOnEvent: The callback to use for when an event is triggered on the path
 
         - Throws: If the inotify_init() file descriptor is less than 0
         - Throws: If the inotify_add_watch(fd, path, mask) returned a file descriptor less than 0 for one of the paths
     */
-    public init(watching path: FilePath, for event: FileSystemEvent) throws {
-        try self.init(watching: [path], for: [event])
+    public init(flags: [InotifyFlag], watching path: FilePath, for event: FileSystemEvent, actionOnEvent callback: @escaping (inotify_event) -> ()) throws {
+        try self.init(flags: flags, watching: [path], for: [event], actionOnEvent: callback)
+    }
+
+    /**
+        Initialize and watch for the specified events on all the paths
+
+        - Parameters:
+            - flag: The inotify flag to use in inotify_init1(flags)
+            - paths: An array of paths to watch
+            - events: An array of the events for which to monitor on each of the paths
+            - actionOnEvent: The callback to use for when an event is triggered on the paths
+
+        - Throws: If the inotify_init() file descriptor is less than 0
+        - Throws: If the inotify_add_watch(fd, path, mask) returned a file descriptor less than 0 for one of the paths
+    */
+    public init(flag: InotifyFlag, watching paths: [FilePath], for events: [FileSystemEvent], actionOnEvent callback: @escaping (inotify_event) -> ()) throws {
+        try self.init(flags: [flag], watching: paths, for: events, actionOnEvent: callback)
+    }
+
+    /**
+        Initialize and watch for the specified events on all the paths
+
+        - Parameters:
+            - flag: The inotify flag to use in inotify_init1(flags)
+            - paths: An array of paths to watch
+            - event: A single event for which to monitor on each of the paths
+            - actionOnEvent: The callback to use for when an event is triggered on the paths
+
+        - Throws: If the inotify_init() file descriptor is less than 0
+        - Throws: If the inotify_add_watch(fd, path, mask) returned a file descriptor less than 0 for one of the paths
+    */
+    public init(flag: InotifyFlag, watching paths: [FilePath], for event: FileSystemEvent, actionOnEvent callback: @escaping (inotify_event) -> ()) throws {
+        try self.init(flags: [flag], watching: paths, for: [event], actionOnEvent: callback)
+    }
+
+    /**
+        Initialize and watch for the specified events on all the paths
+
+        - Parameters:
+            - flag: The inotify flag to use in inotify_init1(flags)
+            - path: The path to watch
+            - events: An array of the events for which to monitor on the path
+            - actionOnEvent: The callback to use for when an event is triggered on the path
+
+        - Throws: If the inotify_init() file descriptor is less than 0
+        - Throws: If the inotify_add_watch(fd, path, mask) returned a file descriptor less than 0 for one of the paths
+    */
+    public init(flag: InotifyFlag, watching path: FilePath, for events: [FileSystemEvent], actionOnEvent callback: @escaping (inotify_event) -> ()) throws {
+        try self.init(flags: [flag], watching: [path], for: events, actionOnEvent: callback)
+    }
+
+    /**
+        Initialize and watch for the specified events on all the paths
+
+        - Parameters:
+            - flag: The inotify flag to use in inotify_init1(flags)
+            - path: The path to watch
+            - event: A single event for which to monitor on the path
+            - actionOnEvent: The callback to use for when an event is triggered on the path
+
+        - Throws: If the inotify_init1() file descriptor is less than 0
+        - Throws: If the inotify_add_watch(fd, path, mask) returned a file descriptor less than 0 for one of the paths
+    */
+    public init(flag: InotifyFlag, watching path: FilePath, for event: FileSystemEvent, actionOnEvent callback: @escaping (inotify_event) -> ()) throws {
+        try self.init(flags: [flag], watching: [path], for: [event], actionOnEvent: callback)
     }
 
     /**
@@ -167,11 +320,12 @@ public struct Inotify {
         - Parameters:
             - path: The path to watch
             - events: The events to watch for
+            - actionOnEvent: The callback to use for when an event is triggered on the path
 
         - Throws: noEvents error if the events array is empty
         - Throws: failedWatch if inotify_add_watch failed to watch
     */
-    public mutating func watch(path: FilePath, for events: [FileSystemEvent]) throws {
+    public mutating func watch(path: FilePath, for events: [FileSystemEvent], actionOnEvent callback: @escaping (inotify_event) -> ()) throws {
         guard !events.isEmpty else {
             throw InotifyError.WatchError.noEvents
         }
@@ -208,7 +362,7 @@ public struct Inotify {
             }
             throw InotifyError.WatchError.unknownWatchFailure(path, flags)
         }
-        watchingDescriptors.append((watchDescriptor, path, flags))
+        watchers.append(Watcher(watchDescriptor, path, flags, callback))
     }
 
     /**
@@ -217,11 +371,12 @@ public struct Inotify {
         - Parameters:
             - path: The path to watch
             - event: The event to watch for
+            - actionOnEvent: The callback to use for when an event is triggered on the path
 
         - Throws: failedWatch if inotify_add_watch failed to watch
     */
-    public mutating func watch(path: FilePath, for event: FileSystemEvent) throws {
-        try self.watch(path: path, for: [event])
+    public mutating func watch(path: FilePath, for event: FileSystemEvent, actionOnEvent callback: @escaping (inotify_event) -> ()) throws {
+        try self.watch(path: path, for: [event], actionOnEvent: callback)
     }
 
     /**
@@ -230,13 +385,14 @@ public struct Inotify {
         - Parameters:
             - paths: The paths to watch
             - events: The events to watch for
+            - actionOnEvent: The callback to use for when an event is triggered on the paths
 
         - Throws: noEvents error if the events array is empty
         - Throws: failedWatch if inotify_add_watch failed to watch
     */
-    public mutating func watch(paths: [FilePath], for events: [FileSystemEvent]) throws {
+    public mutating func watch(paths: [FilePath], for events: [FileSystemEvent], actionOnEvent callback: @escaping (inotify_event) -> ()) throws {
         for path in paths {
-            try self.watch(path: path, for: events)
+            try self.watch(path: path, for: events, actionOnEvent: callback)
         }
     }
 
@@ -249,29 +405,29 @@ public struct Inotify {
         - Throws: failedUnwatch when inotify_rm_watch(fd, wd) fails (Only happens if the file or watch descriptor is invalid, which this library should prevent from happening)
     */
     public mutating func unwatch(path p: FilePath) throws {
-        guard let index = self.watchingDescriptors.index(where: { (_, path, _) in
-            return path == p
+        guard let index = self.watchers.index(where: { (watcher) in
+            return watcher.path == p
         }) else {
             throw InotifyError.UnwatchError.unwatchPathNotFound(p)
         }
 
-        let (descriptor, _, _) = self.watchingDescriptors[index]
+        let watcher = self.watchers[index]
         // This really shouldn't ever throw. The only way this throws is if the
         // inotify or watch descriptor is invalid.
-        guard inotify_rm_watch(self.fileDescriptor, descriptor) == 0 else {
+        guard inotify_rm_watch(self.fileDescriptor, watcher.descriptor) == 0 else {
             if let error = lastError() {
                 switch error {
                 case .EBADF:
                     throw InotifyError.badFileDescriptor(self.fileDescriptor)
                 case .EINVAL:
-                    throw InotifyError.UnwatchError.invalidWatch_OR_FileDescriptor(descriptor, self.fileDescriptor)
+                    throw InotifyError.UnwatchError.invalidWatch_OR_FileDescriptor(watcher.descriptor, self.fileDescriptor)
                 default:
                     throw InotifyError.UnwatchError.unknownUnwatchFailure(p)
                 }
             }
             throw InotifyError.UnwatchError.unknownUnwatchFailure(p)
         }
-        self.watchingDescriptors.remove(at: index)
+        self.watchers.remove(at: index)
     }
 
     /**
