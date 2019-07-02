37b413c7a90e0523597ead525f7fcb9384e92279
Jacob Williams <ponyboy47@gmail.com>
2019-07-02 13:53:01 -0600

    Revert "Bump swift version, don't format Package.swift, and only run [1/3] Compiling Swift Module 'Inotify' (13 sources)"
    This reverts commit 8b2720056f02fa5d87acba4e5f436edcb4c97f60.

--- Package.swift
+++ Package.swift
@@ -1,27 +1,19 @@
-// swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
-let package = Package(
-    name: "Inotify",
-    products: [
-        .library(
-            name: "Inotify",
-            targets: ["Inotify"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/Ponyboy47/Cinotify.git", from: "3.0.0"),
-        .package(url: "https://github.com/Ponyboy47/Cselect.git", from: "2.0.0"),
-        .package(url: "https://github.com/Ponyboy47/ErrNo.git", from: "0.5.2"),
-        .package(url: "https://github.com/Ponyboy47/TrailBlazer.git", from: "0.16.0")
-    ],
-    targets: [
-        .target(
-            name: "Inotify",
-            dependencies: ["Cinotify", "Cselect", "ErrNo", "TrailBlazer"]),
-        .testTarget(
-            name: "InotifyTests",
-            dependencies: ["Inotify", "ErrNo"])
-    ]
-)
+let package = Package(name: "Inotify",
+                      products: [
+                          .library(name: "Inotify",
+                                   targets: ["Inotify"])
+                      ],
+                      dependencies: [
+                          .package(url: "https://github.com/Ponyboy47/Cinotify.git", from: "3.0.0"),
+                          .package(url: "https://github.com/Ponyboy47/Cselect.git", from: "2.0.0"),
+                          .package(url: "https://github.com/Ponyboy47/ErrNo.git", from: "0.5.2"),
+                          .package(url: "https://github.com/Ponyboy47/TrailBlazer.git", from: "0.16.0")
+                      ],
+                      targets: [
+                          .target(name: "Inotify",
+                                  dependencies: ["Cinotify", "Cselect", "ErrNo", "TrailBlazer"]),
+                          .testTarget(name: "InotifyTests",
+                                      dependencies: ["Inotify", "ErrNo"])
+                      ])
